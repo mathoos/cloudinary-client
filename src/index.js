847f12326ef1609440d9e207f7731daee5ebfce5
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-10-21 14:29:57 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import { store } from "./utilities/Store"
+import { setToken } from "./utilities/Slice";
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Chargement initial du token depuis le localStorage
+const storedToken = localStorage.getItem('token');
+if (storedToken) {
+    store.dispatch(setToken(storedToken));
+}
+
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Form from '../components/Form';
+
+
+function Home() {
+
+    return (
+        <div>           
+            <Form/>
+        </div>
+    )
+}
+
+
+export default Home;
