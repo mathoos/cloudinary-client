52247afae22798443dae51a72f6100fc9012d26a
Mathilde Bret <77267445+mathoos@users.noreply.github.com>
2024-11-02 09:32:33 +0100

    signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,7 +5,7 @@ import Logo from "../img/logo.svg";
 import MailIcon from "../img/mail.svg";
 import PasswordIcon from "../img/password.svg";
 
-import './Home.scss';
+import './Signup.scss';
 
 function Signup() {
 
@@ -18,6 +18,7 @@ function Signup() {
 
     const [emailError, setEmailError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
+    const [nameError, setNameError] = useState(false);
     const [message, setMessage] = useState("");
 
     const navigate = useNavigate();
@@ -38,6 +39,7 @@ function Signup() {
 
         setEmailError(false);
         setPasswordError(false);
+        setNameError(false);
         setMessage("");
 
         const passwordError = validatePassword(password);
@@ -54,7 +56,8 @@ function Signup() {
                 setTimeout(() => {
                     navigate("/");
                 }, 3000);  
-            } else {
+            } 
+            else {
                 setMessage("Un problème est survenu lors de l'inscription.");
             }
         } 
@@ -65,45 +68,87 @@ function Signup() {
     };
 
     return (
-        <div className="home">           
+        <div className="signup">           
 
-            <div className="home_title">
+            <div className="signup_title">
                 <h1>Lorem Ipsum is simply dummy text.</h1>
                 <p>
                     Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                     when an unknown printer took a galley of type.
                 </p>
-                <figure className="home_title-logo">
+                <figure className="signup_title-logo">
                     <img src={Logo} alt="Logo"/>
                 </figure>
             </div>
 
-            <div className="home_login">
-                <form className="home_login-form" onSubmit={handleSubmit}>
+            <div className="signup_login">
+                <form className="signup_login-form" onSubmit={handleSubmit}>
                     <h2>Créer un compte</h2>
-                    <div className="home_login-form--fieldset">
-                         <fieldset>
-                            <label htmlFor="nom">Nom</label>
-                            <input type="text" id="nom" name="nom" required onChange={(e) => setNom(e.target.value)} />
-                        </fieldset>
+                    <div className="signup_login-form--fieldset">
+
+                        <div className="row">
+                            <fieldset>
+                                <label htmlFor="nom">Nom</label>
+                                <div className="input-container">
+                                    <input 
+                                        className={`input-field ${nameError ? 'error' : ''}`} 
+                                        type="text" 
+                                        id="nom" 
+                                        name="nom" 
+                                        onChange={(e) => setNom(e.target.value)} 
+                                    />
+                                </div> 
+                            </fieldset>
+                            <fieldset>
+                                <label htmlFor="prenom">Prénom</label>
+                                <div className="input-container">
+                                    <input 
+                                        className={`input-field ${nameError ? 'error' : ''}`} 
+                                        type="text" 
+                                        id="prenom" 
+                                        name="prenom" 
+                                        onChange={(e) => setPrenom(e.target.value)} 
+                                    />
+                                </div>
+                            </fieldset>
+                        </div>
 
                         <fieldset>
-                            <label htmlFor="prenom">Prénom</label>
-                            <input type="text" id="prenom" name="prenom" required onChange={(e) => setPrenom(e.target.value)} />
+                            <div className="row">
+                                <legend>Je suis</legend>
+                                <label className="radio-label">
+                                    <input 
+                                        type="radio" 
+                                        name="genre" 
+                                        value="homme" 
+                                        onChange={(e) => setGenre(e.target.value)} 
+                                    />
+                                    <span class="checkmark"></span>
+                                    Un homme
+                                </label>
+                                <label className="radio-label">
+                                    <input 
+                                        type="radio" 
+                                        name="genre" 
+                                        value="femme" 
+                                        onChange={(e) => setGenre(e.target.value)} 
+                                    />
+                                    <span class="checkmark"></span>
+                                    Une femme
+                                </label>
+                            </div>
                         </fieldset>
-
-                        <fieldset>
-                            <label htmlFor="genre">Genre</label>
-                            <select id="genre" name="genre" onChange={(e) => setGenre(e.target.value)}>
-                                <option value="homme">Homme</option>
-                                <option value="femme">Femme</option>
-                            </select>
-                        </fieldset>
+                        
 
                         <fieldset>
                             <label htmlFor="email">Email</label>
                             <div className="input-container">
-                                <input className={`input-field ${emailError ? 'error' : ''}`} type="email" id="email" name="email" required onChange={(e) => setEmail(e.target.value)}/>
+                                <input 
+                                    className={`input-field ${emailError ? 'error' : ''}`} 
+                                    type="email" 
+                                    id="email" 
+                                    name="email" 
+                                    onChange={(e) => setEmail(e.target.value)}/>
                                 <figure className="icon">
                                     <img src={MailIcon} alt="Mail"/>
                                 </figure>
@@ -113,7 +158,12 @@ function Signup() {
                         <fieldset>
                             <label htmlFor="password">Mot de passe</label>
                             <div className="input-container">
-                                <input className={`input-field ${passwordError ? 'error' : ''}`} type="password" id="password" name="password" required onChange={(e) => setPassword(e.target.value)}/>
+                                <input 
+                                    className={`input-field ${passwordError ? 'error' : ''}`} 
+                                    type="password" 
+                                    id="password" 
+                                    name="password" 
+                                    onChange={(e) => setPassword(e.target.value)}/>
                                 <figure className="icon">
                                     <img src={PasswordIcon} alt="Password"/>
                                 </figure>
@@ -123,13 +173,17 @@ function Signup() {
                             )}
                         </fieldset>
 
-                        {/* Champ de fichier pour l'image de profil */}
                         <fieldset>
                             <label htmlFor="profileImage">Image de profil</label>
-                            <input type="file" id="profileImage" name="profileImage" onChange={(e) => setProfileImage(e.target.files[0])} />
+                            <input 
+                                type="file" 
+                                id="profileImage" 
+                                name="profileImage" 
+                                onChange={(e) => setProfileImage(e.target.files[0])} />
                         </fieldset>
                     </div>
-                    <div className="home_login-form--bouton">
+
+                    <div className="signup_login-form--bouton">
                         <button className="bouton" type="submit">S'inscrire</button>
                     </div>  
                 </form>
